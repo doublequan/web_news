a99f13e9482aa03173d16bb6cfcbfbd35cb8f6cc
billdqu <billdqu@gmail.com>
2016-10-11 19:20:15 -0400

    update

--- web/web_scrapy/scrapy_repeat.sh
+++ web/web_scrapy/scrapy_repeat.sh
@@ -8,7 +8,9 @@ time=$(date "+%Y-%m-%d %H:%M")
 echo "${time}: start run crawler 1p3" >> /code/logs/shell.log
 
 # cd /code/web_scrapy
-/usr/local/bin/scrapy crawl 1p3
+#/usr/local/bin/scrapy crawl 1p3
+/usr/local/bin/scrapy crawl reddit
+/usr/local/bin/scrapy crawl wd_blog
 
 time=$(date "+%Y-%m-%d %H:%M")
 echo "${time}: finish crawling" >> /code/logs/shell.log
