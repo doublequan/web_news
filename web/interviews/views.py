8a93eb02e8521c554fbb5493fe5f9af789d3952a
billdqu <billdqu@gmail.com>
2016-10-11 22:15:08 -0400

    update

--- web/interviews/views.py
+++ web/interviews/views.py
@@ -52,7 +52,7 @@ def index(request):
                    "Tech",
                    "Reddit",
                    "CMU",
-                   "Internet",
+                   "Google",
                    )
 
     boards = []
